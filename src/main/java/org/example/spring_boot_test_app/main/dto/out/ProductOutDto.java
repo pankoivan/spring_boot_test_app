3d378c1d0bd6d0b683e6d06015fd577332cf1cf5
pankoivan <panivan98@yandex.ru>
2024-08-26 13:32:17 +0300

    Добавил поля в out-DTO сущностей.

--- src/main/java/org/example/spring_boot_test_app/main/dto/out/ProductOutDto.java
+++ src/main/java/org/example/spring_boot_test_app/main/dto/out/ProductOutDto.java
@@ -1,4 +1,16 @@
 package org.example.spring_boot_test_app.main.dto.out;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ProductOutDto {
+
+    private String authorUsername;
+
+    private String name;
+
+    private String description;
+
 }
