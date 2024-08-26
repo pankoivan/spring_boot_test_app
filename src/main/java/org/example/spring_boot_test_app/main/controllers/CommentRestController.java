d35bd282264527c22c86c6aeec5ed67c4d9e9001
pankoivan <panivan98@yandex.ru>
2024-08-26 12:37:31 +0300

    Добавил черновики REST-контроллеров.

---
+++ src/main/java/org/example/spring_boot_test_app/main/controllers/CommentRestController.java
@@ -0,0 +1,8 @@
+package org.example.spring_boot_test_app.main.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentRestController {
+
+}
