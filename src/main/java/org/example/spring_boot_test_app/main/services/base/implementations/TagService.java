ac4288b4e27db0a817ad93f7a0fd783e89633413
pankoivan <panivan98@yandex.ru>
2024-08-27 16:22:46 +0300

    Реализовал сервис по работе с тегами.

--- src/main/java/org/example/spring_boot_test_app/main/services/base/implementations/TagService.java
+++ src/main/java/org/example/spring_boot_test_app/main/services/base/implementations/TagService.java
@@ -8,12 +8,14 @@ import org.example.spring_boot_test_app.main.entities.Tag;
 import org.example.spring_boot_test_app.main.exceptions.EntityNoFoundException;
 import org.example.spring_boot_test_app.main.repository.TagRepository;
 import org.example.spring_boot_test_app.main.services.access.implementations.TagAccessService;
+import org.example.spring_boot_test_app.main.services.auxiliary.interfaces.CurrentUserService;
 import org.example.spring_boot_test_app.main.services.base.interfaces.common.BaseService;
 import org.example.spring_boot_test_app.main.services.validation.implementations.TagValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Service
@@ -26,6 +28,8 @@ public class TagService implements BaseService<Tag, TagAddingDto, TagEditingDto>
     private TagAccessService accessService;
 
     private TagValidationService validationService;
+
+    private CurrentUserService currentUserService;
 
     @Override
     public Tag findById(Integer id) {
@@ -41,17 +45,33 @@ public class TagService implements BaseService<Tag, TagAddingDto, TagEditingDto>
 
     @Override
     public Tag add(TagAddingDto addingDto, BindingResult bindingResult) {
-        return null;
+        validationService.addingValidation(addingDto, bindingResult);
+        accessService.shouldCreate();
+        return repository.save(
+                Tag
+                        .builder()
+                        .name(addingDto.getName())
+                        .author(currentUserService.appUser())
+                        .creationDate(LocalDateTime.now())
+                        .build()
+        );
     }
 
     @Override
     public Tag edit(TagEditingDto editingDto, BindingResult bindingResult) {
-        return null;
+        Tag tag = validationService.editingValidation(editingDto, bindingResult);
+        accessService.shouldEdit(tag);
+        tag.setName(editingDto.getName());
+        tag.setEditingDate(LocalDateTime.now());
+        return repository.save(tag);
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        Tag tag = findById(id);
+        validationService.deletionValidation(tag);
+        accessService.shouldDelete(tag);
+        repository.deleteById(id);
     }
 
 }
