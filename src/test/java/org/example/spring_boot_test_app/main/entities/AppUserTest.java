1e4ee5ee2eda6ed1e01e5a248f49499cf87eb859
pankoivan <panivan98@yandex.ru>
2024-08-31 15:37:33 +0300

    Добавил проверку реализаций true/false-методов от UserDetails в тест пользователя.

--- src/test/java/org/example/spring_boot_test_app/main/entities/AppUserTest.java
+++ src/test/java/org/example/spring_boot_test_app/main/entities/AppUserTest.java
@@ -1,6 +1,7 @@
 package org.example.spring_boot_test_app.main.entities;
 
 import org.example.spring_boot_test_app.main.entities.enums.Role;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -16,6 +17,26 @@ class AppUserTest {
         AppUser appUser = new AppUser();
         appUser.setRole(role);
         assertThat(appUser.getAuthorities()).isEqualTo(Collections.singletonList(role));
+    }
+
+    @Test
+    void testIsAccountNonExpired_alwaysTrue() {
+        assertThat(new AppUser().isAccountNonExpired()).isTrue();
+    }
+
+    @Test
+    void testIsAccountNonLocked_alwaysTrue() {
+        assertThat(new AppUser().isAccountNonLocked()).isTrue();
+    }
+
+    @Test
+    void testIsCredentialsNonExpired_alwaysTrue() {
+        assertThat(new AppUser().isCredentialsNonExpired()).isTrue();
+    }
+
+    @Test
+    void testIsEnabled_alwaysTrue() {
+        assertThat(new AppUser().isEnabled()).isTrue();
     }
 
 }
